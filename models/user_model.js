a5f462c01dbd4e293f98d96db42502dabd161c8f
Sayed Anwar <sayedanwarofficial@gmail.com>
2024-11-16 10:31:54 +0600

    hash password system and jwt token added

--- models/user_model.js
+++ models/user_model.js
@@ -25,4 +25,37 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// hash password
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+
+    user.password = hashedPassword;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// jwt token
+userSchema.methods.generateToken = async function () {
+  try {
+    const user = this;
+    const token = jwt.sign(
+      { userId: user._id, isAdmin: user.isAdmin },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+    return token;
+  } catch (err) {
+    res.status(404).send({ message: err.message });
+  }
+};
+
 module.exports = mongoose.model("User", userSchema);
